134055e47bebb903fb895a21ec6d2b5ece53270f
M. Yoga Nugraha <myoganugraha@gmail.com>
2024-11-29 16:31:17 +0700

    feat: add findMessageById

--- src/main/kotlin/com/nugraha/restfulWebService/controller/MessageController.kt
+++ src/main/kotlin/com/nugraha/restfulWebService/controller/MessageController.kt
@@ -4,6 +4,7 @@ import com.nugraha.restfulWebService.dto.Message
 import com.nugraha.restfulWebService.service.MessageService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -19,4 +20,12 @@ class MessageController(private val service: MessageService) {
         val savedMessage = service.save(message)
         return ResponseEntity.created(URI("/${savedMessage.id}")).body(savedMessage)
     }
+
+    @GetMapping("/{id}")
+    fun getMessageById(@PathVariable id: String): ResponseEntity<Message> =
+        service.findMessageById(id).toResponseEntity()
+
+    private fun Message?.toResponseEntity(): ResponseEntity<Message> = this?.let {
+        ResponseEntity.ok(it)
+    } ?: ResponseEntity.notFound().build()
 }
--- src/main/kotlin/com/nugraha/restfulWebService/service/MessageService.kt
+++ src/main/kotlin/com/nugraha/restfulWebService/service/MessageService.kt
@@ -2,6 +2,7 @@ package com.nugraha.restfulWebService.service
 
 import com.nugraha.restfulWebService.dto.Message
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.query
 import org.springframework.stereotype.Service
 import  java.util.UUID
 
@@ -10,6 +11,10 @@ class MessageService(private val db: JdbcTemplate) {
     fun findMessages(): List<Message> = db.query("select * from messages") {
         response, _ -> Message(response.getString("id"), response.getString("text"))
     }
+
+    fun findMessageById(id: String): Message? = db.query("select * from messages where id = ?", id) {
+        response, _ -> Message(response.getString("id"), response.getString("text"))
+    }.singleOrNull()
 
     fun save(message: Message): Message {
         val id = message.id ?: UUID.randomUUID().toString()
