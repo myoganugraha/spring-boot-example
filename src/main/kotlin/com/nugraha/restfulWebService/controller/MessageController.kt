ef30baef781bdcb5f754a48cc7d5db9c4705cb4b
M. Yoga Nugraha <myoganugraha@gmail.com>
2024-11-29 15:23:03 +0700

    feat: integrate with MessageService

--- src/main/kotlin/com/nugraha/restfulWebService/controller/MessageController.kt
+++ src/main/kotlin/com/nugraha/restfulWebService/controller/MessageController.kt
@@ -1,15 +1,23 @@
 package com.nugraha.restfulWebService.controller
 
 import com.nugraha.restfulWebService.dto.Message
+import com.nugraha.restfulWebService.service.MessageService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
 
 @RestController
-class MessageController {
+class MessageController(pivate val service: MessageService) {
     @GetMapping("/")
-    fun listMessages() = listOf(
-        Message("1", "hello"),
-        Message("2", "bonjour"),
-        Message("3", "privet"),
+    fun listMessages() = service.findMessages()
     )
+
+    @PostMapping
+    fun post(@RequestBody message: Message): ResponseEntity<Message> {
+        val savedMessage = service.save(message)
+        return ResponseEntity.created(URI("/${savedMessage.id}")).body(savedMessage)
+    }
 }
